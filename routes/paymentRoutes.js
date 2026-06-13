27f77a787c40ea21a5cd8b58fa95654bb05c1b69
pointfinauto-collab <pointfinauto@gmail.com>
2026-06-13 17:13:20 +0300

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { createPayment, getMyPayments, getReceipt } = require('../controllers/paymentController');
+const { createPayment, createPaypalOrder, capturePaypalOrder, getMyPayments, getReceipt } = require('../controllers/paymentController');
 const { protect, requireClient } = require('../middleware/auth');
 const { validate, paymentValidation } = require('../middleware/validators');
 
 router.use(protect(), requireClient);
 
 router.post('/', paymentValidation, validate, createPayment);
+router.post('/paypal/create-order', createPaypalOrder);
+router.post('/paypal/capture-order', capturePaypalOrder);
 router.get('/', getMyPayments);
 router.get('/:id/receipt', getReceipt);
 
